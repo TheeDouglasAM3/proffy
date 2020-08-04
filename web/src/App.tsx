2b670d950c69b80e3f5fd94ed12408f572ff0799
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-04 14:21:16 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Landing from './pages/Landing'
+import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Landing />
   );
 }
 
