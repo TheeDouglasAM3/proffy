4beeb691e8cace77c8c04b53f440988d42179462
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-04 17:51:32 -0300

    Update TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css'
 
@@ -29,34 +28,9 @@ function TeacherList(){
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars2.githubusercontent.com/u/33377432?s=460&u=7cc319a628dc40f856b4d2e0d8121160ac0c532f&v=4" alt="Douglas"/>
-                        <div>
-                            <strong>Douglas</strong>
-                            <span>Matemática</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Entusiasta das melhores tecnologias de matemática avançada
-                        <br/><br/>
-                        Está há dois anos tentando contar de 1 à 1 milhão .
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/hora 
-                            <strong>R$ 50,00</strong>
-                        </p>
-
-                        <button type="button">
-                            <img src={whatsappIcon} alt="whatsapp"/>
-
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
