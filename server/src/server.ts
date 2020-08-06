884f5bd7057b50e2d38f5cd8199a7c24d67bb66a
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-06 20:29:33 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'  
-import routes from './routes';
+import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
