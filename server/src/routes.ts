59e62833d9d81350dc0f87e7f98217f8599318a9
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-06 20:29:11 -0300

    Update routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express, { request, response } from 'express'
 import ClassesController from './controllers/ClassesController'
+import ConnectionsController from './controllers/ConnectionsController'
 
 const routes = express.Router()
 const classesController = new ClassesController()
+const connectionsController = new ConnectionsController()
 
-routes.post('/classes', classesController.store)
 routes.get('/classes', classesController.index)
+routes.post('/classes', classesController.store)
+
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.store)
 
 export default routes
