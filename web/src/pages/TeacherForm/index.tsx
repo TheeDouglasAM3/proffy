28b40b857a2a1d4591de579ff611b6a14341ea8e
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-09 21:03:15 -0300

    Update TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,7 +4,31 @@ import PageHeader from '../../components/PageHeader'
 function TeacherForm(){
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas." />
+            <PageHeader 
+                title="Que incrível que você quer dar aulas." 
+                description="O primeiro passo é você preencher este formulário de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados.</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">WhatsApp</label>
+                        <input type="text" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
         </div>
     )
 }
