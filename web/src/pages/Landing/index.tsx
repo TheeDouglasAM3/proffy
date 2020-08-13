b05da07b9d667f3446e30f6d2afd500600444a17
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-13 17:11:36 -0300

    Update Landing page with integration the server

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -10,6 +11,16 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('connections')
+      .then(response => {
+        const { total } = response.data
+        setTotalConnections(total)
+      })
+  }, [])
+
   return (
     <div id="page-landing">
       <div className="container" id="page-landing-content">
@@ -36,7 +47,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões realizadas.{" "}
+          Total de {totalConnections} conexões realizadas.{" "}
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
