f29dbb71f3c4b65195b637af66c18407c66d772c
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-13 21:35:02 -0300

    Update api.ts to works Heroku server

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    //baseURL: 'http://localhost:3333',
+    baseURL: 'https://proffy-theedouglasam.herokuapp.com/',
 })
 
 export default api
